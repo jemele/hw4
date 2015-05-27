a4676c90a4ea86f919eaff410a309cac10864f52
Joshua Emele <jemele@acm.org>
2015-05-26 22:05:55 -0700

    hw4: sketch irobot zed/bbb processor
    We'll need to fill in the blanks for both bbb/zed message processing and irobot
    processing.

--- hw3/hw3.sdk/SDK/SDK_Export/pl_uart_test_0/src/helloworld.c
+++ hw3/hw3.sdk/SDK/SDK_Export/pl_uart_test_0/src/helloworld.c
@@ -1,6 +1,7 @@
 // Joshua Emele <jemele@acm.org>
 // Tristan Monroe <twmonroe@eng.ucsd.edu>
 #include <stdio.h>
+#include <stdlib.h>
 #include "platform.h"
 #include "irobot.h"
 #include "uart.h"
@@ -17,6 +18,34 @@ int DEBUG_getc_nowait()
         return XUartPs_ReadReg(STDIN_BASEADDRESS, XUARTPS_FIFO_OFFSET) & 0xff;
     }
     return EOF;
+}
+
+// Process console input.
+static void process_console()
+{
+    if (!XUartPs_IsReceiveData(STDIN_BASEADDRESS)) {
+        return;
+    }
+    const u8 c = XUartPs_ReadReg(STDIN_BASEADDRESS, XUARTPS_FIFO_OFFSET) & 0xff;
+    putchar(c);
+    switch (c) {
+    case 'Q': printf("goodbye\n"); exit(0);
+    }
+}
+
+// Process bbb input.
+static void process_bbb(uart_axi_t *device)
+{
+    if (uart_axi_recv_ready(device)) {
+        const u8 c = uart_axi_recv(device);
+        putchar(c);
+    }
+}
+
+// Process irobot tasks.
+// XXX Add sensor poll task.
+static void process_irobot(irobot_t *device)
+{
 }
 
 int main()
@@ -50,34 +79,17 @@ int main()
         return status;
     }
 
-    // Read a message from the serial.
-    // Validate the message is well formed and process the message.
-    int tx = 0;
+    // Read and process messages from the serial until told to quit.
     for (;;) {
 
-        // process local console input.
-        int c = DEBUG_getc_nowait();
-        if (EOF != c) {
-            if (c == 'Q') {
-                return 0;
-            }
-            if (!tx) {
-                printf("--\npl-uart tx: ");
-                tx = 1;
-            }
-            uart_axi_send(&uart, c);
-            putchar(c);
-        }
+        // process console input.
+        process_console();
 
-        // process axi uart input.
-        if (uart_axi_recv_ready(&uart)) {
-            if (tx) {
-                printf("--\npl-uart rx: ");
-                tx = 0;
-            }
-            c = uart_axi_recv(&uart);
-            putchar(c);
-        }
+        // process bbb input.
+        process_bbb(&uart);
+
+        // process irobot tasks.
+        process_irobot(&irobot);
     }
 
     return 0;
