55a6c76c77d14b169f6175646f9b893d35255eb7
Joshua Emele <jemele@acm.org>
2015-05-29 12:59:24 -0700

    hw4: bbb: Handle partial reads and read timeouts robustly
    The serial link can be *very* flaky.

--- bbb/zed-client.c
+++ bbb/zed-client.c
@@ -8,17 +8,58 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/select.h>
 #include "bbb.h"
+
+#define SERIAL_DEVICE "/dev/ttyO1"
+
+static int read_serial(int fd, void *b, int n, int timeout_ms)
+{
+    fd_set set;
+    FD_ZERO(&set);
+    FD_SET(fd, &set);
+    struct timeval timeout = {
+        .tv_sec = timeout_ms/1000,
+        .tv_usec = 1000 * (timeout_ms%1000),
+    };
+
+    // Wait until character data is available.
+    int status = select(fd+1, &set, 0, 0, &timeout);
+    if (status == -1) {
+        fprintf(stderr, "select failed %d\n", errno);
+        return errno;
+    }
+    if (status == 0) {
+        fprintf(stderr, "select timeout\n");
+        return -1;
+    }
+
+    // Set up the read to block until all characters are received or timeout.
+    struct termios topt;
+    tcgetattr(fd, &topt);
+    topt.c_cc[VMIN] = n;
+    topt.c_cc[VTIME] = timeout_ms/100;
+    tcsetattr(fd,TCSANOW,&topt);
+
+    // Finally, read and validate.
+    status = read(fd, b, n);
+    if ((status == -1) || (status < n)) {
+        fprintf(stderr, "read failed %d %d\n", status, errno);
+        return errno;
+    }
+    return status;
+}
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s device\n", argv[0]);
-        return EINVAL;
-    }
-    int fd = open(argv[1], O_RDWR|O_NOCTTY);
+    int fd = open(SERIAL_DEVICE, O_RDWR|O_NOCTTY);
     if (fd == -1) {
         fprintf(stderr, "open failed %s %s\n", argv[1], strerror(errno));
+        return errno;
+    }
+    int status = tcflush(fd, TCIFLUSH);
+    if (status == -1) {
+        fprintf(stderr, "tcflush %d\n", errno);
         return errno;
     }
 
@@ -28,31 +69,37 @@ int main(int argc, char **argv)
         .version = bbb_header_version_value,
         .id = bbb_id_sensor_read,
     };
-    int status = write(fd, &header, sizeof(header));
+    status = write(fd, &header, sizeof(header));
     if (status == -1) {
         fprintf(stderr, "write failed %s\n", strerror(errno));
         return errno;
     }
-    tcdrain(fd);
+    if (status != sizeof(header)) {
+        fprintf(stderr, "write failed %d %d\n", status, sizeof(header));
+        return EIO;
+    }
 
-    // Remember, our interface takes time to work.
-    usleep(1000*50);
-
-    status = read(fd, &header, sizeof(header));
-    if ((status == -1) || (status < sizeof(header))) {
+    // Read the header.
+    printf("reading response header\n");
+    status = read_serial(fd, &header, sizeof(header), 750);
+    if (status != sizeof(header)) {
         fprintf(stderr, "read failed %d %d\n", status, errno);
         return errno;
     }
+
+    // Print the header.
     printf("magic %x version %x id %d\n",
             header.magic, header.version, header.id);
     if (header.id != bbb_id_sensor_data) {
         printf("invalid message %d\n", header.id);
         return;
     }
+
+    // Read the message.
     bbb_id_sensor_data_t data;
-    status = read(fd, &data, sizeof(data));
-    if (status == -1) {
-        fprintf(stderr, "read failed %s\n", strerror(errno));
+    status = read_serial(fd, &data, sizeof(data), 750);
+    if (status != sizeof(data)) {
+        fprintf(stderr, "read failed %d %d\n", status, errno);
         return errno;
     }
     printf("bumper %d wall %d\n",
